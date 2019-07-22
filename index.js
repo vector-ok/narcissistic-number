1e08a0d95e41fe45299db35c7eca2c3be9705a88
vector-ok <52209467+vector-ok@users.noreply.github.com>
2019-07-22 16:06:55 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,59 @@
+//Author: Victor Okpon
+//email: vikzy2rhyme@yahoo.com
+//phone:  0706 340 3892
+
+// window.addEventListener('DOMContentLoaded', () => {}
+
+let numPowered = [];
+let sum;
+
+// variables for styling result
+let isValid = document.getElementById('isValid');
+let notValid = document.getElementById('notValid');
+isValid.style.display = "none";
+notValid.style.display = "block";
+notValid.innerHTML = "testing Narcissistic tendency...";
+
+
+const calc = () => {
+  let yourNum = document.getElementById('yourNum');
+  let inputValue = document.getElementById('inputValue');
+  let input = document.getElementById('input').value;
+  const numLength = input.length;
+
+  yourNum.innerHTML = "You entered";
+  inputValue.innerHTML = input;
+
+  // isolate numbers, perform maths, get Nazi..
+
+  for (let i = 0; i < input.length; i++) {
+    numPowered[i] = Math.pow(input[i], numLength);
+    sum += numPowered[i];
+
+    }
+
+    // logging sum to console to see if everything is in order
+
+    console.log(sum);
+
+    // display message depending on result
+
+    if(input == ""){
+      yourNum.innerHTML = "Please enter a number";
+    }
+
+    else if(input == sum){
+      notValid.style.display = "none";
+      isValid.style.display = "block";
+      isValid.innerHTML = "CONFIRMED! \ Your number is full blown Narcissistic";
+    }
+    else{
+      notValid.style.display = "block";
+      isValid.style.display = "none";
+      notValid.innerHTML = "Sorry, your number isn't Narcissistic";
+    }
+
+    // resetting sum so it doesn't run crazy
+
+    sum = 0;
+}
